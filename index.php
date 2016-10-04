3e47c4e4a1a875c7dde78c0dd20759efbee70a86
ÉLio Laender <laenderquadros@gmail.com>
2016-10-04 17:43:43 -0300

    Atualizado exemplo de saída

--- index.php
+++ index.php
@@ -28,3 +28,23 @@ $retorno = $objOU->advertencia()
 
 #exibindo na tela o resultado do encadeamento.
 echo $retorno;
+
+/*
+Saída:
+
+- Atenção Pelotão Alfa!
+- Marche!
+- Atenção Pelotão Alfa!
+- Alto!
+- Atenção Pelotão Alfa!
+- Esquerda volver!
+- Atenção Pelotão Alfa!
+- Meia volta volver!
+- À vontade!
+- Descansar!
+Notificações: 
+* Comando numero 9 [à vontade] não pode ser executado, somente executado a partir de [descansar] 
+* Comando numero 10 [descansar] não pode ser executado, somente executado a partir de [sentido] 
+
+
+*/
